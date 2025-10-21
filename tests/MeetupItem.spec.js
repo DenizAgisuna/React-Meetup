20342d18aca044316d4437354eb0e1c1ecdeaff8
DenizAgisuna <146776477+DenizAgisuna@users.noreply.github.com>
2025-10-21 15:09:31 +0200

    Fixed Playwright test
     - Fixed Playwright test to scroll up for the navbar to reappear

--- tests/MeetupItem.spec.js
+++ tests/MeetupItem.spec.js
@@ -15,7 +15,10 @@ await page.getByRole('textbox', { name: 'Address' }).press('Tab');
 await page.getByRole('textbox', { name: 'Description' }).fill('Lorem ipsum');
 await page.getByRole('button', { name: 'Add Meetup' }).click();
 await page.getByRole('listitem').filter({ hasText: 'Meetup Title' }).getByRole('button').click();
-await page.getByRole('link', { name: 'My Favorites1' }).click();
+await page.evaluate(() => {
+  window.scrollTo(0, 0);
+});
+await page.getByRole('link', { name: 'My Favorites' }).click();
 await page.getByRole('button', { name: 'Remove from favorites' }).click();
 await page.locator('[data-test="app"] div').filter({ hasText: 'Favorites PageYou don\'t have' }).click();
 await page.getByRole('link', { name: 'All Meetups' }).click();
@@ -24,6 +27,7 @@ await page.getByRole('listitem').filter({ hasText: 'This is a third' }).getByRol
 await page.getByRole('listitem').filter({ hasText: 'This is a second' }).getByRole('button').click();
 await page.getByRole('button', { name: 'Add to favorites' }).click();
 await page.getByRole('link', { name: 'My Favorites4' }).click();
+
 await page.getByRole('listitem').filter({ hasText: 'This is a first meetupMeetupstreet 5, 12345 Meetup CityThis is a first, amazing' }).getByRole('button').click();
 await page.getByRole('listitem').filter({ hasText: 'This is a second' }).getByRole('button').click();
 await page.getByRole('listitem').filter({ hasText: 'This is a third' }).getByRole('button').click();
